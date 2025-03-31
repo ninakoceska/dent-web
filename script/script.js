2eaf8e90b8697a72a5506ac84209ea2a08e75d2d
ninakoceska <108902059+ninakoceska@users.noreply.github.com>
2025-03-31 06:53:29 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -34,25 +34,26 @@ function myFunction() {
 }
 
 window.onload=function(){
-    document.getElementById("pp-frame").style.display = "none";
+    document.getElementById("pp-frame").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11774.780570472481!2d21.392103736362266!3d41.99795772629826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6c0dddbd261fe607%3A0xd615f4221bd98d5e!2z0JTRgC4g0I_QsNC80LHQsNC30L7QstGB0LrQuCAtIERyLiBEemFtYmF6b3Zza2k!5e0!3m2!1smk!2smk!4v1733450276467!5m2!1smk!2smk";
 }
 
 function locationSK() {
-    document.getElementById("pp-frame").style.display = "none";
-    document.getElementById("sk-frame").style.display = "flex";
+    document.getElementById("pp-frame").src="";
+    document.getElementById("pp-frame").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11774.780570472481!2d21.392103736362266!3d41.99795772629826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6c0dddbd261fe607%3A0xd615f4221bd98d5e!2z0JTRgC4g0I_QsNC80LHQsNC30L7QstGB0LrQuCAtIERyLiBEemFtYmF6b3Zza2k!5e0!3m2!1smk!2smk!4v1733450276467!5m2!1smk!2smk";
+    // document.getElementById("sk-frame").style.display = "flex";
 
-    document.getElementById("info-sk").style.display = "grid";
+    document.getElementById("info-sk").style.display = "flex";
     document.getElementById("info-pp").style.display = "none";
 
 
 }
 
 function locationPP() {
-    document.getElementById("pp-frame").style.display = "flex";
-    document.getElementById("sk-frame").style.display = "none";
+    document.getElementById("pp-frame").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3279.204169083656!2d21.54599161709484!3d41.34402980291249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1356ebe0de86137b%3A0x214aff6a3c29fbca!2z0JTRgC4g0I_QsNC80LHQsNC30L7QstGB0LrQuCAtIERyLiBEemFtYmF6b3Zza2k!5e0!3m2!1smk!2smk!4v1733450607750!5m2!1smk!2smk";
+    // document.getElementById("sk-frame").style.display = "none";
 
     document.getElementById("info-sk").style.display = "none";
-    document.getElementById("info-pp").style.display = "grid";
+    document.getElementById("info-pp").style.display = "flex";
 
 }
 
